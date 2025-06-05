413143d49288f19cf7db76399e9e0976ad4aa21b
johny dotsville <come.as@you.are>
2025-06-05 09:42:54 +0300

    Исправил ошибки в компоненте, отображающем markdown

--- src/components/Task/TaskDescription.tsx
+++ src/components/Task/TaskDescription.tsx
@@ -1,27 +1,9 @@
-import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import gfm from 'remark-gfm';
-import { visit } from 'unist-util-visit';
 import ClipboardIcon from '@mui/icons-material/FileCopy';
 import { Box, Button } from '@mui/material';
+import Markdown from 'react-markdown';
 
-
-const syntaxHighlightPlugin = () => tree => {
-  visit(tree, 'code', node => {
-    if (!node.lang || !node.value) return;
-    node.type = 'html';
-    node.value = `<pre><code class="${node.lang}">${node.value}</code></pre>`;
-  });
-};
-
-const CustomComponent = ({ value, language }) => {
-  return (
-    <SyntaxHighlighter style={dracula} language={language}>
-      {value}
-    </SyntaxHighlighter>
-  );
-};
 
 const handleCopyToClipboard = async (text) => {
   try {
@@ -33,36 +15,40 @@ const handleCopyToClipboard = async (text) => {
 
 
 export const TaskDescription = ({ description }) => {
+  console.log(description)
   return (
-    <ReactMarkdown 
-      children={description} 
+    <Markdown
+      children={description}
       components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(?<lang>.*)/.exec(className ?? '');
-            let lang = '';
-            if (match && match.groups?.lang) {
-              lang = match.groups.lang;
-            }
-            return !inline ? (
-              <Box position='relative'>
-                <Button 
-                  onClick={() => handleCopyToClipboard(String(children))}
-                  aria-label="Копировать"
-                  size="small"
-                  color='warning'
-                  sx={{ position: 'absolute', top: '1rem', right: '0.25rem' }}
-                >
-                  <ClipboardIcon />
-                </Button>
-                <CustomComponent value={String(children)} language={lang} />
-              </Box>
-            ) : (
-              <code className={className}>{children}</code>
-            );
-          },
-        }}
-        remarkPlugins={[gfm]}
-        rehypePlugins={[syntaxHighlightPlugin]}
+        code(props) {
+          const {children, className, node, ...rest} = props
+          const match = /language-(\w+)/.exec(className || '')
+          return match ? (
+            <Box position='relative'>
+              <Button 
+                onClick={() => handleCopyToClipboard(String(children))}
+                aria-label="Копировать"
+                size="small"
+                color='warning'
+                sx={{ position: 'absolute', top: '1rem', right: '0.25rem' }}
+              >
+                <ClipboardIcon />
+              </Button>
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={ dracula }
+              />
+            </Box>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          )
+        }
+      }}
     />
   )
 }
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Stack, Typography } from "@mui/material"
 import DataArrayIcon from '@mui/icons-material/DataArray';
 
 export const TopMenu = () => {
