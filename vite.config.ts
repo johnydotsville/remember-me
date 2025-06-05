079968b074b7b8e07b6746bd7173e288b4907652
johny dotsville <come.as@you.are>
2025-06-05 10:23:39 +0300

    Пробуем для githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'remember me'
+        }
+      }
+    })
+  ],
+  build: {
+    outDir: 'docs'
+  },
 })
