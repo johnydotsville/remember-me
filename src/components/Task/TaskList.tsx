1993e4855a1f47d5cadf9c7535121309753cf03c
johny dotsville <come.as@you.are>
2025-07-15 16:42:59 +0300

    Заменил спойлер в списке задач на обычную плашку

--- src/components/Task/TaskList.tsx
+++ src/components/Task/TaskList.tsx
@@ -1,27 +1,15 @@
-import { TaskView } from "@/src/components/Task/TaskView";
-import { SpoilerGroup } from "../SpoilerGroup";
-import { Stack, Tooltip } from "@mui/material";
-import CheckIcon from '@mui/icons-material/Check';
+import { Stack } from "@mui/material";
+import { TaskStripe } from "./TaskStripe";
 
 
 export const TaskList = ({ tasks, doneTasks }) => {
-  const taskList = tasks.map(task => ({
-    id: task.id,
-    title:
-      <Tooltip 
-        title={<>
-          <div>{task.path}</div>
-          <div>{task.tags?.join(', ') || 'Тэги не заданы'}</div>
-        </>} 
-        enterDelay={2000}
-      >
-        <Stack direction='row' justifyContent='space-between'>
-          <span>{task.title || task.name}</span>
-          {doneTasks.includes(task.id) && <CheckIcon sx={{ color: 'green' }} />}
-        </Stack>
-      </Tooltip>,
-    content: <TaskView task={task} />
-  }));
-
-  return <SpoilerGroup items={taskList} spacing="0.5rem" />
+  return (
+    <Stack direction='column' gap={1} sx={{ cursor: 'pointer' }}>
+      { 
+        tasks.map(task => 
+          <TaskStripe key={task.id} task={task} done={doneTasks.includes(task.id)} />
+        ) 
+      }
+    </Stack>
+  )
 }
---
+++ src/components/Task/TaskStripe.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "@mui/material";
+import CheckIcon from '@mui/icons-material/Check';
+
+
+export function TaskStripe({ task, done = false }) {
+  return (
+    <Stack sx={{
+      border: '1px solid green',
+    }} padding={1} direction='row' gap={1}>
+      { done && <CheckIcon sx={{ color: 'green' }} />}
+      { task.title || task.name }
+    </Stack>
+  )
+}
